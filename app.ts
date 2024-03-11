2084a5c8b2a56c7fccf7060cdf8f8230c0c13944
diopmorfall <morfall.papydiop99@gmail.com>
2024-03-11 11:01:58 +0000

    String, number, boolean, type assignment and inference

--- app.ts
+++ app.ts
@@ -10,3 +10,13 @@ function somma(a: number, b: number) {
   //* once it's programmed and it won't throw any error
   //* the .ts files will be compiled into a .js file
   
+  //* TS allows us to avoid dynamic behaviors like these (which are correct in JS)
+  /*
+    let prova = 7;
+    prova = 'cniwc'
+  */
+  
+  let test: number = 5; //* specific type assignment
+  let a: string; //* either do this, or give the value directly
+  let inference = 9; //* type inference, TS guesses the type from the value
+  let isTrue: boolean;
